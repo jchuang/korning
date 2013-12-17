69aa0856cfa934638dea70bb60c2abd929099aa2
Janet Chuang <janet.chuang@gmail.com>
2013-12-16 22:49:03 -0500

    add product controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Korning::Application.routes.draw do
+
+  resources :products, only: [:index]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
